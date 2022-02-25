87ed80401114d1782a0f1f673914b62da25a8c0d
George Madeley <82886454+George-Madeley@users.noreply.github.com>
2022-02-25 23:05:31 +0000

    Improved AI Start Word

--- node.py
+++ node.py
@@ -1,12 +1,16 @@
+import string
+from AlphabetStat import AlphabetStat
+
 class Node:
     def __init__(self, word):
         self.word = word
-        self.nextNode = None
+        self.score = 0
+        self.nextNode:Node = None
 
     def __str__(self):
         return self.word
 
-    def SetWord(self, word):
+    def SetWord(self, word:string):
         self.word = word
 
     def GetWord(self):
@@ -21,8 +25,8 @@ class Node:
     def IsWordValid(self):
         return len(self.word) == 5
 
-    def IsLetterInWord(self, letter):
-        letterIndex = self.word.find(str(letter))
+    def hasLetter(self, letter:string):
+        letterIndex = self.word.find(letter)
         return letterIndex >= 0
 
     def GetLetterIndex(self, goalLetter):
@@ -30,3 +34,17 @@ class Node:
             if goalLetter == letter:
                 return index
         return -1
+
+    def CalculateCharacterOccurrences(self, characterStats):
+        for index, letter in enumerate(self.word):
+            if letter == characterStats.GetCharacter():
+                characterStats.IncrementStatAtIndex(index)
+
+    def ResetScore(self):
+        self.score = 0
+
+    def GetScore(self):
+        return self.score
+
+    def IncreaseScoreBy(self, amount):
+        self.score += amount
